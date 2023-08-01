a34746667842375153e6ad17c0fcd6f1c3d2d0b8
Andrew Sapronov <andrew.sapronov@gmail.com>
2023-08-01 09:28:55 +0300

    [nba810] Removed obsolete modules

--- debian-bsps/aurora-810/nb_ports_controller/thrift/src/rpc_server.h
+++
@@ -1,8 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-extern int start_rpc_server(void);
-extern int stop_rpc_server(void);
-#ifdef __cplusplus
-}
-#endif
--- debian-bsps/aurora-810/nb_ports_controller/unit_test/unit_test.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UNIT_TEST_H
-#define _UNIT_TEST_H
-
-#include <stdint.h>
-
-int BSP_behavior_test(int test);
-
-#endif
