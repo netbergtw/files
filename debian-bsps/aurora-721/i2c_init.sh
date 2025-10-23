cccefaa3ee1c5b45737751534b947d9df2cb0f0e
Andrew Sapronov <andrew.sapronov@gmail.com>
2025-10-23 14:54:02 +0300

    [nba721] Check i2c roots order

--- debian-bsps/aurora-721/i2c_init.sh
+++ debian-bsps/aurora-721/i2c_init.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
-# Netberg Aurora 721 basic init 
+#Netberg Aurora 721 basic init 
+
+#Default behavior 
+I2C_ISMT_ROOT=0 #0x19ac
+I2C_I801_ROOT=1 #0x19df
+
+if [[ "0x19df" == $(cat /sys/bus/i2c/devices/i2c-0/device/device) ]]; then
+    I2C_ISMT_ROOT=1
+    I2C_I801_ROOT=0
+fi
+
+echo "I2C_ISMT_ROOT=$I2C_ISMT_ROOT, I2C_I801_ROOT=$I2C_I801_ROOT"
 
 #Load modules
 modprobe i2c-mux-pca954x
 modprobe optoe
 
 #Init CPU_HOST_I2C
-echo pca9548 0x70 > /sys/bus/i2c/devices/i2c-0/new_device 
-echo '-2' > /sys/bus/i2c/devices/0-0070/idle_state
+echo pca9548 0x70 > /sys/bus/i2c/devices/i2c-$I2C_ISMT_ROOT/new_device
+echo '-2' > /sys/bus/i2c/devices/$I2C_ISMT_ROOT-0070/idle_state
 
 ##SYSEEPROM
-echo 24c32 0x57 > /sys/bus/i2c/devices/i2c-0/new_device
+echo 24c32 0x57 > /sys/bus/i2c/devices/i2c-$I2C_ISMT_ROOT/new_device
 sleep 1
-hexdump -n 256 -C /sys/bus/i2c/devices/0-0057/eeprom
+hexdump -n 256 -C /sys/bus/i2c/devices/$I2C_ISMT_ROOT-0057/eeprom
 
 
 #Init CPU_LEG_I2C
 ## SFP root
-echo pca9548 0x72 > /sys/bus/i2c/devices/i2c-1/new_device
-echo '-2' > /sys/bus/i2c/devices/1-0072/idle_state
+echo pca9548 0x72 > /sys/bus/i2c/devices/i2c-$I2C_I801_ROOT/new_device
+echo '-2' > /sys/bus/i2c/devices/$I2C_I801_ROOT-0072/idle_state
 
 ##QSFP28 leafs
 for I in {10..13}; 
